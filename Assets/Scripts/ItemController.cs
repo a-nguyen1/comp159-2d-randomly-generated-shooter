66abb26fd18137b3c948e31cc879c6bc4b6c61c6
Leah <l_hernandez23@u.pacific.edu>
2023-12-01 12:18:36 -0800

    created private variable pickedUpItem

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -12,7 +12,7 @@ public class ItemController : MonoBehaviour
     {
         Neighbor,
         Gun,
-        Health
+        Health,
     }
     // Start is called before the first frame update
     void Start()
